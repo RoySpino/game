e756b4c9b684b4340cc470ca064644d5764cd65e
Roy <spinosaurus001@hotmail.com>
2017-01-22 16:31:39 -0800

    remove redundent texture function

--- GLPrint.vb
+++ GLPrint.vb
@@ -82,8 +82,6 @@ Public Class GLPrint
     End Sub
 
     Sub rectangle(charIndex As Integer, str As String)
-        getTextureCorordinat(str)
-
         'vl.Add(size * charIndex)
         'vl.Add(0)
         GL.Vertex3((size * charIndex), 0.0F, 0.0F)
@@ -105,44 +103,4 @@ Public Class GLPrint
         GL.TexCoord2(texcoords(6), texcoords(7))
     End Sub
 
-    Sub getTextureCorordinat(kar As String)
-        Dim val As Integer = Asc(kar)
-        Dim modVal, intDivVal As Double
-
-        If (val Mod 16) = 0 Then
-            modVal = 0
-        Else
-            modVal = 1.0 / (val Mod 16)
-        End If
-
-        If CInt(val / 16) = 0 Then
-            intDivVal = 0
-        Else
-            intDivVal = 1.0 / CInt(val / 16)
-        End If
-
-        texcoords(0) = modVal
-        texcoords(1) = intDivVal
-        'tl.Add(texcoords(0))
-        'tl.Add(texcoords(1))
-
-        texcoords(2) = modVal + 0.0625
-        texcoords(3) = intDivVal
-        'tl.Add(texcoords(2))
-        'tl.Add(texcoords(3))
-
-        texcoords(4) = modVal + 0.0625
-        texcoords(5) = intDivVal + 0.0625
-        'tl.Add(texcoords(4))
-        'tl.Add(texcoords(5))
-
-        texcoords(6) = modVal
-        texcoords(7) = intDivVal + 0.0625
-        'tl.Add(texcoords(6))
-        'tl.Add(texcoords(7))
-
-        'nl.Add(0)
-        'nl.Add(1)
-        'nl.Add(0)
-    End Sub
 End Class
